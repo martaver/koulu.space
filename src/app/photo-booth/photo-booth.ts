72d6e182483286acc290a02a3bd229d9c4107bd2
Sebastian Nemeth <sebastian@semidavinci.com>
2016-07-23 00:15:40 +0300

    Image capture is working (but needs tweaking)

--- src/app/photo-booth/photo-booth.ts
+++ src/app/photo-booth/photo-booth.ts
@@ -2,43 +2,86 @@
  * Created by sebas_000 on 20/07/2016.
  */
 
-import {Component, ElementRef} from '@angular/core';
+import {Component, ElementRef, AfterViewInit} from '@angular/core';
+
+interface VideoElement {
+
+  src: string;
+  play()
+}
+
+interface CanvasElement {
+
+  drawImage(videoElement:VideoElement, number:number, number2:number):void;
+
+  toDataURL(s2:string):string;
+  getContext(s2:string):any;
+}
+
+enum State {
+  Recording,
+  Confirming
+}
 
 @Component({
   selector: 'photo-booth',
   styles: [`
-  video {
-    width: 100%;
-  }
+    video {
+      width: 100%;
+    }
+    canvas {
+      display: none;
+    }
+    img {
+      width: 100%;
+    }
 `],
-  template: `<video muted autoplay></video>`
-})
-export class PhotoBooth {
+  template: `
 
-  videoElement: Element;
+<div *ngIf="state == State.Recording" id="recorder">
+  <video muted autoplay (click)="snapshot()"></video>
+  <canvas></canvas>
+  Testing
+</div>
+
+<div *ngIf="state == State.Confirming" id="confirmer">
+  <img [src]="imgSrc">
+</div>
+
+`
+})
+export class PhotoBooth  implements AfterViewInit {
+
+
+  public State = State;
+  state: State = State.Recording;
+  videoElement: VideoElement;
+  canvasElement: CanvasElement;
   navigator: any;
   window: any;
-
+  imgSrc: string;
+  private ctx;
 
   constructor(public element: ElementRef) {
 
 
   }
 
-  ngOnInit() {
-
-    this.videoElement = this.element.nativeElement.querySelector('video');
-
-    this.window = window;
-    this.navigator = window.navigator;
-    this.navigator.getUserMedia = this.navigator.getUserMedia || this.navigator.webkitGetUserMedia || this.navigator.mozGetUserMedia || this.navigator.msGetUserMedia;
+  ngAfterViewInit() {
 
     this.start();
   }
 
   public start() {
 
+    this.videoElement = this.element.nativeElement.querySelector('#recorder').querySelector('video');
+
+    this.window = window;
+    this.navigator = window.navigator;
+    this.navigator.getUserMedia = this.navigator.getUserMedia || this.navigator.webkitGetUserMedia || this.navigator.mozGetUserMedia || this.navigator.msGetUserMedia;
+
     if (this.window.stream) {
+
       this.videoElement.src = null;
       if(this.window.stream.stop) this.window.stream.stop();
     }
@@ -61,5 +104,24 @@ export class PhotoBooth {
       console.log('navigator.getUserMedia error: ', error);
 
     });
+  }
+
+
+  public snapshot() {
+
+    if(this.window.stream) {
+
+      this.canvasElement = this.element.nativeElement.querySelector('#recorder').querySelector('canvas');
+      this.ctx = this.canvasElement.getContext('2d');
+
+      this.ctx.drawImage(this.videoElement, 0, 0);
+      // "image/webp" works in Chrome.
+      // Other browsers will fall back to image/png.
+      this.imgSrc = this.canvasElement.toDataURL('image/webp');
+
+      console.log('imgSrc', this.imgSrc);
+
+      this.state = this.State.Confirming;
+    }
   }
 }
