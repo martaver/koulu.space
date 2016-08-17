2cfc47eaf00bd3523e611a0c02fa23b938a13130
Sebastian Nemeth <sebastian@ilixir.fi>
2016-08-17 17:43:54 +0300

    Fixed problem where non-ios camera images would have their orientation messed up. Added load-image library to handle all EXIF orientation tags and transform accordingly.

--- src/app/teach/teach-selfie.ts
+++ src/app/teach/teach-selfie.ts
@@ -4,6 +4,8 @@
 import {Component, EventEmitter, Output, ViewChild, NgZone, ElementRef} from '@angular/core';
 import {PhotoBooth, GotSnapshotEvent} from "../photo-booth/photo-booth";
 
+declare var loadImage: any;
+
 @Component({
   selector: 'teach-selfie',
   styleUrls: [ './teach.style.css' ],
@@ -22,7 +24,7 @@ import {PhotoBooth, GotSnapshotEvent} from "../photo-booth/photo-booth";
       
       <div *ngIf="iOS" id="photo-booth-ios">
         
-        <img [src]="dataUrl" *ngIf="dataUrl">        
+        <img [src]="dataUrl" *ngIf="dataUrl">
         
         <div class="take-picture constrain-ratio-4-3" *ngIf="!(dataUrl)">
           <label class="constrain-ratio-content"> <!--Technique for styling file upload inputs: http://stackoverflow.com/questions/21842274/cross-browser-custom-styling-for-file-upload-button-->
@@ -64,6 +66,7 @@ export class TeachSelfie{
   private hasSnapshot: boolean;
   private snapshot: GotSnapshotEvent;
   private dataUrl: string;
+  private orientation: any;
 
   constructor(private zone: NgZone) {
 
@@ -105,46 +108,68 @@ export class TeachSelfie{
 
   private onInputChanged(event) {
 
-    var input = event.target;
-    if (input.files && input.files[0]) {
+    loadImage.parseMetaData(event.target.files[0], data => { this.zone.run(() => {
 
-      var reader = new FileReader();
+        this.orientation = data.exif ? data.exif.get('Orientation') : 1;
 
-      reader.onload = (e) => {
+        var options = {
+          orientation: this.orientation,
+          canvas: true
+        };
 
-        var canvas: any = document.getElementById("photo-booth-ios-canvas");
-        var context: any = canvas.getContext("2d");
+        var handleLoad = canvas => { this.zone.run(() => {
 
-        // store current data to an image
-        var img = new Image();
-        img.src = e.target['result'];
+          this.dataUrl = canvas.toDataURL('image/png');
+          this.snapshot = new GotSnapshotEvent(this.dataURItoBlob(this.dataUrl), this.dataUrl);
+          this.hasSnapshot = true;
+        })};
 
-        img.onload = () => {
-
-          console.log('image loaded');
-          // reset the canvas with new dimensions
-          canvas.width = img.height;
-          canvas.height = img.width;
-
-          console.log('drawing w x h: ', img.width, img.height);
-
-          context.translate(img.height, 0);
-          context.rotate(Math.PI / 2);
-          context.drawImage(img, 0, 0);
-
-          this.zone.run(() => {
+        loadImage(event.target.files[0], handleLoad, options);
+      });
+    });
 
 
-            // this.dataUrl = e.target['result'];
-            this.dataUrl = canvas.toDataURL('image/png');
-            this.snapshot = new GotSnapshotEvent(this.dataURItoBlob(this.dataUrl), this.dataUrl);
-            this.hasSnapshot = true;
-          })
-        }
-      };
 
-      reader.readAsDataURL(input.files[0]);
-    }
+    // var input = event.target;
+    // if (input.files && input.files[0]) {
+    //
+    //   var reader = new FileReader();
+    //
+    //   reader.onload = (e) => {
+    //
+    //     var canvas: any = document.getElementById("photo-booth-ios-canvas");
+    //     var context: any = canvas.getContext("2d");
+    //
+    //     // store current data to an image
+    //     var img = new Image();
+    //     img.src = e.target['result'];
+    //
+    //     img.onload = () => {
+    //
+    //       console.log('image loaded');
+    //       // reset the canvas with new dimensions
+    //       canvas.width = img.height;
+    //       canvas.height = img.width;
+    //
+    //       console.log('drawing w x h: ', img.width, img.height);
+    //
+    //       context.translate(img.height, 0);
+    //       context.rotate(Math.PI / 2);
+    //       context.drawImage(img, 0, 0);
+    //
+    //       this.zone.run(() => {
+    //
+    //
+    //         // this.dataUrl = e.target['result'];
+    //         this.dataUrl = canvas.toDataURL('image/png');
+    //         this.snapshot = new GotSnapshotEvent(this.dataURItoBlob(this.dataUrl), this.dataUrl);
+    //         this.hasSnapshot = true;
+    //       })
+    //     }
+    //   };
+    //
+    //   reader.readAsDataURL(input.files[0]);
+    // }
   }
 
 
